91bc2d51295bf7257b2178477136d27443c401f0
Lowwwe <Peter.lowe@itcarlow.ie>
2019-11-05 21:21:14 +0000

    added helicopter sprite

--- helicopter/helicopter/Game.cpp
+++ helicopter/helicopter/Game.cpp
@@ -112,7 +112,7 @@ void Game::render()
 {
 	m_window.clear(sf::Color::White);
 	m_window.draw(m_welcomeMessage);
-	m_window.draw(m_logoSprite);
+	m_window.draw(m_heloSprite);
 	m_window.display();
 }
 
@@ -141,11 +141,12 @@ void Game::setupFontAndText()
 /// </summary>
 void Game::setupSprite()
 {
-	if (!m_logoTexture.loadFromFile("ASSETS\\IMAGES\\SFML-LOGO.png"))
+	if (!m_heloTexture.loadFromFile("ASSETS\\IMAGES\\helicopter.png"))
 	{
 		// simple error message if previous call fails
 		std::cout << "problem loading logo" << std::endl;
 	}
-	m_logoSprite.setTexture(m_logoTexture);
-	m_logoSprite.setPosition(300.0f, 180.0f);
+	m_heloSprite.setTexture(m_heloTexture);
+	m_heloSprite.setPosition(300.0f, 180.0f);
+	m_heloSprite.setTextureRect(sf::IntRect{ 0,0,180,64 });
 }
--- helicopter/helicopter/Game.h
+++ helicopter/helicopter/Game.h
@@ -34,10 +34,11 @@ private:
 	sf::RenderWindow m_window; // main SFML window
 	sf::Font m_ArialBlackfont; // font used by message
 	sf::Text m_welcomeMessage; // text used for message on screen
-	sf::Texture m_logoTexture; // texture used for sfml logo
-	sf::Sprite m_logoSprite; // sprite used for sfml logo
-
 	
+
+	sf::Texture	m_heloTexture;
+	sf::Sprite m_heloSprite;
+	sf::Vector2f m_heloPosition;
 	bool m_exitGame; // control exiting game
 
 };
